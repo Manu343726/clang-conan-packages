975e19389cb6433119ba4c791cbe957f8df14233
Manu343726 <Manu343726@gmail.com>
2018-05-17 09:14:28 +0200

    Use conan-package-tools for package generation
    Also generate version 3.8.0 until the package sources are fixed

--- ci_generator.py
+++ ci_generator.py
@@ -5,7 +5,7 @@ import yaml, sys
 def eprint(*args, **kwargs):
         print(*args, file=sys.stderr, **kwargs)
 
-def generate_gitlab_package(gitlab_ci, packages, compiler, version, user, channel):
+def generate_gitlab_package(gitlab_ci, remote, packages, compiler, version, user, channel):
     job = "{}_{}_{}".format('+'.join(template["packages"]), version, compiler)
     build_job = "build_" + job
     test_job = "test_" + job
@@ -14,7 +14,8 @@ def generate_gitlab_package(gitlab_ci, packages, compiler, version, user, channe
     gitlab_ci[build_job] = {
         "tags": ["linux", "docker"],
         "image": "lasote/conan" + compiler,
-        "script": ["CONAN_VERSION_OVERRIDE={} conan create {} {}/{}".format(version, package, user, channel) for package in packages]
+        "script": ["pushd {package} && CONAN_VERSION_OVERRIDE={version} CONAN_USERNAME={user} CONAN_REFERENCE={package}/{version} CONAN_CHANNEL={channel} python3 ../build.py && popd".format(package=package, user=user, version=version, channel=channel) for package in packages] +
+                  ["conan upload {}/{}@{}/{} -r {} --all".format(package, version, user, channel, remote) for package in packages]
     }
 
 
@@ -22,13 +23,14 @@ def generate_gitlab(template):
     gitlab_ci = {}
 
     gitlab_ci['before_script'] = [
+        "pip install conan_package_tools --user",
         "conan remote add {} {}".format(template["remote"]["name"], template["remote"]["url"]),
         "conan user {} -p ${} -r {}".format(template["remote"]["user"], template["remote"]["password"], template["remote"]["name"])
     ]
 
     for compiler in template["compilers"]:
         for version in template["versions"]:
-            generate_gitlab_package(gitlab_ci, template["packages"], compiler, version, template["channel"]["user"], template["channel"]["channel"])
+            generate_gitlab_package(gitlab_ci, template["remote"]["name"], template["packages"], compiler, version, template["channel"]["user"], template["channel"]["channel"])
 
     yaml.dump(gitlab_ci, open('.gitlab-ci.yml', 'w'), default_flow_style = False)
 
